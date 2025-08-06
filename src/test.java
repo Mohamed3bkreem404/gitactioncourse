e14f806240ad9a32d42532cb9ee7d63b678dc42b
Mohamed3bkreem404 <neutmoha@gmail.com>
2025-08-06 15:26:11 +0300

    testo two

--- src/test.java
+++ src/test.java
@@ -5,5 +5,6 @@ public class test {
     public static void main(String[] args) {
             System.out.println("YEllo world");
             System.out.println("Absloute cinema");
+            System.out.println("Absloute cinema for the second time");
     }
 }
